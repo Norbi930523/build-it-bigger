eac3273684ce8a2d2d0910532a8d899b39d97b4d
Norbert Boros <boros.norbert@freemail.hu>
2018-03-26 22:58:37 +0200

    Setup Google Cloud Endpoints module

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,12 +3,13 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.udacity.norbi930523.builditbigger.jokegenerator.JokeGenerator;
 
 import javax.inject.Named;
 
 /** An endpoint class we are exposing */
 @Api(
-        name = "myApi",
+        name = "jokesApi",
         version = "v1",
         namespace = @ApiNamespace(
                 ownerDomain = "backend.builditbigger.gradle.udacity.com",
@@ -18,12 +19,21 @@ import javax.inject.Named;
 )
 public class MyEndpoint {
 
+    private static final JokeGenerator JOKE_GENERATOR = new JokeGenerator();
+
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
+        return response;
+    }
+
+    @ApiMethod(name = "joke", httpMethod = "GET")
+    public MyBean joke() {
+        MyBean response = new MyBean();
+        response.setData(JOKE_GENERATOR.getJoke());
         return response;
     }
 
